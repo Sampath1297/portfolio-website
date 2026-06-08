e3f808bbd25283769ebacabef5d975cd51c5a23e
Sampath Polisetti <sampathpolisetti1297@gmail.com>
2026-06-08 11:37:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const darkModeBtn = document.getElementById("darkModeBtn");
 
+darkModeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+
+    if (name.trim() === "") {
+        document.getElementById("message").innerText =
+            "Please enter your name.";
+    } else {
+        document.getElementById("message").innerText =
+            "Thank you, " + name + "!";
+    }
+});
